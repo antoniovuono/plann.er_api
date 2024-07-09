d0c4ef28d47920c964c3594dec468f11029ac8da
Antonio Vuono <antoniosvuono@icloud.com>
2024-07-08 22:21:48 -0300

    feature: server updated

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import fastify from 'fastify';
 import { createTrip } from './routes/create-trip';
 import cors from "@fastify/cors";
-
 import { serializerCompiler, validatorCompiler} from "fastify-type-provider-zod";
 import { confirmTrip } from './routes/confirm-trip';
 
 const app = fastify()
+
+app.register(cors, {
+    origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
